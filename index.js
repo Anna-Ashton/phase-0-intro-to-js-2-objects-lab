f1173ffe097b8d8096fdbf92cf00f6e23a681966
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-08 14:40:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ann",
+    streetAddress: "Machakos",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value,};
+
+    return newEmployee;
+}
+const newEmployee = {
+    employee, key, value
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const deleteEmployee = {...employee};
+
+    delete deleteEmployee[key];
+
+    return deleteEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
